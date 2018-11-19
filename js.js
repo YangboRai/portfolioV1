3d6294e7316086254b4f808c35cbc74fcc250df8
Yangbo Rai <45079677+YangboRai@users.noreply.github.com>
2018-11-19 21:16:42 +0000

    Add files via upload

---
+++ js.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('.btn').click(function(){
+	 $('.two-2').css("display","block");
+	});
+
+
+	$('.menu').click(function(){
+		$('nav').toggleClass('active');
+	});
+
+
+
+});
+
